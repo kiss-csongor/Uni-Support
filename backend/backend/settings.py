0a1cecb118bca8e59bd03faa66ad661e4832fd04
Csongor Kiss <70952337+kiss-csongor@users.noreply.github.com>
2025-02-20 13:12:13 +0100

    Jenkins database teszt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,13 +89,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'UniSupportDatabase',
+        'USER': 'kisscsongor',  
+        'PASSWORD': 'Csongi2004.',
+        'HOST': 'uni-jenkins.sytes.net',   
+        'PORT': '5432',     
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
